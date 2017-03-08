0fc9c11517ba0cb5d86e6e365e83b1ef3a4893c5
Thanh Nguyen <thanh.nguyen@mtech.com.vn>
2017-03-08 18:07:54 +0700

    comit p1

---
+++ Assets/Script/Auto.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Auto : MonoBehaviour {
+    public float speed;
+    public float huongquay =1;
+    // Use this for initialization
+    void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        //chuyển động xoay
+        //Debug.Log(transform.rotation.z);
+       
+        // nếu >30 do thi quay nguoc lai
+        if(transform.rotation.z > 0.26f)
+        {
+            huongquay = -1;
+            speed = huongquay * speed;
+        }
+
+        if (transform.rotation.z < -0.26f)
+        {
+            huongquay = -1;
+            speed = huongquay * speed;
+        }
+        this.transform.Rotate(0, 0, speed);
+    }
+}
---
+++ Assets/Script/Vatcan.cs
@@ -0,0 +1,38 @@
+﻿using UnityEngine;
+using System.Collections;
+using System;
+
+public class Vatcan : MonoBehaviour {
+    public float speed_Down = 0;
+    public float posion_max = -2.6f;
+    public float posion_min = -4.6f;
+
+     bool Play { get;  set; }
+
+    // Use this for initialization
+    void Start () {
+        float x = UnityEngine.Random.Range(posion_min, posion_max);
+        transform.localPosition = new Vector3(x, transform.localPosition.y, 0);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (Input.GetMouseButtonDown(0) && !Play)
+            Play = true;
+        if (transform.localPosition.y < -6.5f)
+            move();
+        if(Play)
+            transform.Translate(Vector3.down * speed_Down * Time.deltaTime);
+   
+           
+	}
+
+    private void move()
+    {
+        float x = UnityEngine.Random.Range(posion_min, posion_max);
+        float y = 6.5f;
+        transform.localPosition = new Vector3(x, y, 0);
+
+    }
+
+}
